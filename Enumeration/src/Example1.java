3c2cc23809733aaf154a6ed2cd15ad6c4345c2b8
Preeti <jhapreeti872@gmail.com>
2025-11-10 22:36:00 +0530

    add

---
+++ Enumeration/src/Example1.java
@@ -0,0 +1,9 @@
+enum Colors {RED, GREEN, BLUE};
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        Colors color = Colors.RED;
+        System.out.println(color);
+    }
+}
