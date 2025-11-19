25b25c0633bdb2870e2a5ed5b9880fcc3197fd75
Preeti Jha <jhapreeti872@gmail.com>
2025-11-19 18:26:22 +0530

    Added String programs

---
+++ String/src/Intro.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Intro {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = "Preeti";
+        String greet = sc.nextLine();
+
+        System.out.println("Hi " + name + " " +greet);
+
+    }
+}
