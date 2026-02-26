d50a812d01722efda5479a15f9e1bd9ae95d7a3d
Preeti Kumari <jhapreeti872@gmail.com>
2026-02-26 23:36:32 +0530

    Refactor Sc1 class to input name, address, and age

--- java/sc1.java
+++ java/sc1.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
-class Sc1 {
-    public static void main(String[] args) {
-        String name, add;
-       Scanner sc = new Scanner(System.in);
-       System.out.println("\nEnter name");
-       name = sc.nextLine();
-       System.out.println("Enter address");
-       add = sc.nextLine();
-       System.out.println("name is :" + name);
-       System.out.println("address is :" + add);
+
+public class input {
+    public static void main(String args[]) {
+        // print name add
+        String name, address;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name");
+        name = sc.nextLine();
+        System.out.println("Enter address");
+        address = sc.nextLine();
+        System.out.println("Enter age");
+        int age = sc.nextInt();
+
+        System.out.println("Hii " + name + "\nyou lives in " + address + "\n your age is : " + age);
+        sc.close();
 
     }
 }
+
