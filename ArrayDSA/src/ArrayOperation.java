c352ab8c5651a341c1b25001b07897e11af506b0
Preeti Kumari <jhapreeti872@gmail.com>
2026-04-27 00:40:05 +0530

    Add files via upload

---
+++ ArrayDSA/src/ArrayOperation.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.Array;
+
+public class ArrayOperation {
+     public static void main(String[] args){
+         int arr = new arr[1,2,3,4,5,6,7,8,9];
+     }
+    static int insert(Array arr){
+
+    }
+
+    static int delete(Array arr){
+
+    }
+
+    static int fetch(Array arr){
+
+    }
+    static int rotate(Array arr){
+         
+    }
+}
