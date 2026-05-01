cc1a7b567652cf098843f5dc3b70d092e73ab41c
Preeti Jha <jhapreeti872@gmail.com>
2026-05-01 23:16:02 +0530

    add new array operation

--- ArrayDSA/src/ArrayInsert.java
+++ ArrayDSA/src/ArrayInsert.java
@@ -1,30 +1,35 @@
 public class ArrayInsert {
+    int count = 0;
+
     public static void main(String[] args) {
 
-        int count = 0;
+        ArrayInsert o = new ArrayInsert();
 
-        InsertElementArray o = new InsertElementArray();
-
-        int a[] = new int[10];
+        int a[] = new int[10]; //creating array of 10 size
 //        a[0] = 10;
 //        a[1] = 20;
 //        a[2] = 30;
 //        a[3] = 40;
 //        a[5] = 50;
 
-  
-        for (int j = 0; j <a.length; j++) {
-           o.insert(a, j);
+//inserting array
+        for (int j = 0; j < a.length; j++) {
+            o.insert(a, j);
         }
 
-
-        for(int i = 0; i <a.length; i++) {
+//printing array
+        for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
     }
 
-    public void insert(int a[], int val){
-        a[count++] = val;
-    }
-
+        public void insert(int[] a, int val){
+        if(count < a.length){
+            a[count] = val;
+            count++;
+        }
+        else{
+            System.out.println("array full");
+        }
+        }
 }
